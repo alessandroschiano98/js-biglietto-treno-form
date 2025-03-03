9dc1625a107fd1ead95509cd58cb54e8f7d1b696
alessandroschiano98 <schianoski98@gmail.com>
2025-03-03 15:02:46 +0100

    varie modifiche

--- js/script.js
+++ js/script.js
@@ -44,8 +44,11 @@ const message = `Il prezzo base è €${baseTicketForKm.toFixed(2)}. Visto che i
 */
 
 // PREPRAZIONE ELEMENTI DOM 
+
+// FORM
 const userForm = document.getElementById("user-form")
 console.log(userForm);
+// FORM INPUTS
 const nameLabel = document.getElementById("name")
 console.log(nameLabel);
 const surnameLabel = document.getElementById("surname")
@@ -54,29 +57,29 @@ const ageLabel = document.getElementById("age")
 console.log(ageLabel);
 const kmLabel = document.getElementById("km")
 console.log(kmLabel);
+// RESULT
 const btnCalcoloTotale = document.getElementById("btnCalcola")
 console.log(btnCalcoloTotale);
 const textTotale = document.getElementById("textTotale")
 console.log(textTotale);
 
+
 /*
-buttonCalcoloTotale.addEventListener("click", function () {
+btnCalcola.addEventListener("btnCalcola", function () {
     const name = nameLabel.value;
     const surname = surnameLabel.value;
-    const age = ageLabel.value;
-    const km = kmLabel.value;
-    console.log(name, surname, age, km);
+    console.log(name, surname);
 });
+
+
 */
 
-userForm.addEventListener("calcola", gestisciUserForm);
+userForm.addEventListener("btnCalcola", gestisciUserForm);
 function gestisciUserForm (event) {
     event.preventDefault();
     console.log("userForm");
     const name = nameLabel.value;
     const surname = surnameLabel.value;
     const fullname = `${name} ${surname}`;
-    const age = ageLabel.value;
-    const km = kmLabel.value;
-    console.log(fullname, age, km, );
+    console.log(fullname);
 }
