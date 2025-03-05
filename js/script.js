ec87680c4194c7084f1f2a365cb1615317d8e156
alessandroschiano98 <schianoski98@gmail.com>
2025-03-05 21:49:17 +0100

    fixato il problema della stampa applicando il "submit"

--- js/script.js
+++ js/script.js
@@ -1,48 +1,3 @@
-/*
-
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
-
-// KM DA PERCORRERE // 
-const userKmStr = prompt("Quanti chilometri vuoi percorrere?")
-const userKm = parseInt(userKmStr)
-console.log(userKm);
-
-
-// ETA' DEL'UTENTE // 
-const userAgeStr = prompt("Mi ricordi quanti anni hai?")
-const userAge = parseInt(userAgeStr)
-console.log(userAge);
-
-/ BIGLIETTO //
-const baseTicketForKm = 0.21;
-const ticket = (0.21 * userKm)
-console.log(ticket);
-
-// ESECUZIONE LOGICA //
-
-// USER AGE SCONTO + SCONTO BIGLIETTO // 
-let saleTicket = 0; /* QUESTO INCLUDE AUTOMATICAMENTE ELSE 
-if (userAge < 18) {
-    saleTicket = 20;
-}
-else if (userAge > 65) {
-    saleTicket = 40;
-}
-
- SI AGGIUNGE Else finale solo nel momento in cui c'è una condizione che non vi è inclusa nelle altre 2 
-
-
-// SCONTO BIGLIETTO // 
-const finalPrice = baseTicketForKm - (baseTicketForKm * saleTicket / 100)
-
-// MESSAGGIO SCONTO //
-const message = `Il prezzo base è €${baseTicketForKm.toFixed(2)}. Visto che il passeggero ha ${userAge} anni, hai il diritto allo sconto ${saleTicket}%. Quindi il prezzo finale è €${finalPrice.toFixed(2)}`
-
-
-*/
-
 // PREPRAZIONE ELEMENTI DOM 
 
 // FORM
@@ -62,19 +17,48 @@ console.log(kmLabel);
 // RESULT
 const btnCalcolaTotale = document.getElementById("btn-calcola")
 console.log(btnCalcolaTotale);
-const boxResultLabel = document.getElementById("boxResult")
+const boxResultLabel = document.getElementById("box-result")
 console.log(boxResultLabel);
+const resultLabel = document.getElementById("result-elem")
 
 
 
-userForm.addEventListener("btn-calcola", function () {
-    console.log("btn-calcola");
+
+
+userForm.addEventListener("submit", gestisciUserForm); // calcola non è un codice scaturito da html
+
+function gestisciUserForm(event) {
+    event.preventDefault();
+    // PRELEVO I VALORI DEGLI INPUT
     const name = nameLabel.value;
-    console.log(name);
     const surname = surnameLabel.value;
-    console.log(surname);
     const age = ageLabel.value;
-    console.log(age);
     const km = kmLabel.value;
-    console.log(km);
-});
+
+
+    // BIGLIETTO //
+    const baseTicketForKm = 0.21;
+    const ticket = (0.21 * kmLabel)
+    console.log(ticket);
+
+    // USER AGE SCONTO + SCONTO BIGLIETTO // 
+    let saleTicket = 0;
+    if (ageLabel < 18) {
+        saleTicket = 20;
+    }
+    else if (ageLabel > 65) {
+        saleTicket = 40;
+    }
+
+    // SCONTO BIGLIETTO // 
+    const finalPrice = baseTicketForKm - (baseTicketForKm * saleTicket / 100)
+
+
+    // INSERISCO I VALORI DEGLI ELEMENTI
+    resultLabel.innerText = `Ciao ${name} ${surname} dato che la tua età corrisponde a ${age} anni ed hai intenzione di percorrere ${km} km, hai il diritto al ${saleTicket}% di sconto, dunque il costo del tuo biglietto sarà di €${finalPrice.toFixed(2)} `;
+
+
+
+
+
+};
